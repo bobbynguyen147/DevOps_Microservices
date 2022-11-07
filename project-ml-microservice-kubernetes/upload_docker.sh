2e7449358296533b3225571dfd066e325ddd5378
LongHoangNguyen <LongHoangNguyen>
2022-11-07 11:25:01 +0700

    squart commit

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="013268759557.dkr.ecr.ap-southeast-1.amazonaws.com/udacity-project-4:latest"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
+aws ecr get-login-password --region ap-southeast-1 | docker.exe login --username AWS --password-stdin 013268759557.dkr.ecr.ap-southeast-1.amazonaws.com
+docker.exe tag longnh30-app:latest $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker.exe push $dockerpath
